8f95568bee2a229102dd7b76b5b15e28369184f8
nikhil <kujurnikhil0007@gmail.com>
2025-11-23 09:27:46 -0800

    Revert "test code"
    This reverts commit 283416cf172546ee9455d971fe918a0827af89a2.

--- simple2.java
+++ simple2.java
@@ -2,6 +2,5 @@ public class simple2 {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Test Hello world");
-        System.out.println("another line");
     }
 }
